afb60a6100384b8233ec62a06ce4aaa16e910408
Elizabeth <Elizabeth@192.168.1.73>
2015-06-07 17:00:17 -0500

    Primera actualizacion

---
+++ src/main/java/unitec/mavenincidencias/Aplicacion.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unitec.mavenincidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ *
+ * @author campitos
+ */
+@ComponentScan
+@EnableAutoConfiguration
+public class Aplicacion {
+    
+    public static void main(String args[]){
+         SpringApplication.run(Aplicacion.class, args);
+    }
+    
+}
